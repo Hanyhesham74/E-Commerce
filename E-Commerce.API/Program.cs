64320a2521ba970eacdabf9060751e0e004f12be
Dell <Dell@DESKTOP-PNCP0EN>
2025-10-25 23:31:38 +0300

    Session1

--- E-Commerce.API/Program.cs
+++ E-Commerce.API/Program.cs
@@ -1,4 +1,8 @@
 
+using Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Presistence.Data;
+
 namespace E_Commerce.API
 {
     public class Program
@@ -13,9 +17,17 @@ namespace E_Commerce.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                // options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            builder.Services.AddScoped<IDataSeading, DataSeading>();
+            //Add-Migration "IntialCreate" -OutputDir Data\Migrations
             var app = builder.Build();
-
+            var scope = app.Services.CreateScope();
+            var objectofdataseading = scope.ServiceProvider.GetRequiredService<IDataSeading>();
+            objectofdataseading.DataSeed();
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
